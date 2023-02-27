3719f0327ebdc537e33fdff09a985435efe9ad84
Sheree1986 <sheree.edmund@gmail.com>
2023-02-27 16:55:21 -0500

    fix error in database.yml

--- config/environments/*.rb
+++
@@ -1,9 +0,0 @@
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  :user_name => '14e9c28e81beb6',
-  :password => '5f72786b5910a0',
-  :address => 'sandbox.smtp.mailtrap.io',
-  :domain => 'sandbox.smtp.mailtrap.io',
-  :port => '2525',
-  :authentication => :cram_md5
-}
